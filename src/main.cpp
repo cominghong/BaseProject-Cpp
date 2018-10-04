36ec6172df3e4a44d16f2dc83087637851f33273
cominghong <cominghong@gmail.com>
2018-10-04 20:06:07 +0800

    add *

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main() {
     printf("Rule Breaker!!!\n");
     printf("5 + 2 = %d\n", addNumber(5, 2));
+    printf("2 * 8 = %d\n", app(2, 8));
     printf("%d\n", getTriangleType(3, 4, 5));
     return 0;
 }
@@ -12,6 +13,11 @@ int main() {
 
 int addNumber(int a, int b) {
     return a + b;
+}
+
+int app(int q,int w)
+{
+    return q * w;
 }
 
 int getTriangleType(int a, int b, int c) {
